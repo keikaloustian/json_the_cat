185c20e864c322f592a8bf3bd79bc818b0232f91
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-29 15:27:56 -0700

    Implement CaaS

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,21 @@
 const request = require('request');
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  
+  // In case of request failure
+  if (error) {
+    console.log('Request error: ', error);
+    process.exit();
+  }
+
+  const data = JSON.parse(body);
+  
+  // If search query is not found
+  if (data[0] === undefined) {
+    console.log('Breed not found');
+    process.exit();
+  }
+  
+  console.log(data[0].description);
+});
